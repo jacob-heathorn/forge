f5b122efb9da41cdf7bc6f4c58ae2576bc680416
Jacob <jacob.heathorn@gmail.com>
2023-09-11 21:39:41 -0700

    Do it

--- platforms/native/platform.cmake
+++ platforms/native/platform.cmake
@@ -4,24 +4,6 @@
 # Include the common platform cmake.
 include(${PROJECT_SOURCE_DIR}/platforms/common/platform.cmake)
 
-# Wrapper for add_executable
-function(App)
-  set(APP_NAME ${ARGV0})
-  add_executable(${ARGN})
-  add_common_c_cxx_flags(${APP_NAME})
+function(platformify target)
+  add_common_c_cxx_flags(${target})
 endfunction()
-
-# Wrapper for add_library
-function(Lib)
-  add_library(${ARGN})
-  set(LIB_NAME ${ARGV0})
-  add_common_c_cxx_flags(${LIB_NAME})
-endfunction()
-
-# TODO
-# # Compiles a unit test executable
-# function(Test)
-#   App(${ARGN})
-#   set(LIB_NAME ${ARGV0})
-#   target_link_libraries(${LIB_NAME} pw_unit_test)
-# endfunction()
