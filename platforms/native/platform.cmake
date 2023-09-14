bc425b56de7e773e7a58002cbe73a4f9e1da0e10
Jacob <jacob.heathorn@gmail.com>
2023-09-13 22:07:16 -0700

    Finish pulling in pw unit test

--- platforms/native/platform.cmake
+++ platforms/native/platform.cmake
@@ -2,16 +2,9 @@
 # across the tree.
 
 # Include the common platform cmake.
-include(${PROJECT_SOURCE_DIR}/platforms/common/platform.cmake)
+include($ENV{FORGE_ROOT}/platforms/common/platform.cmake)
 
 # Adds platform-specific libraries and options to the target.
 function(platformify target)
   add_common_c_cxx_flags(${target})
-endfunction()
-
-# Compiles a pigweed unit test executable
-function(add_pw_test)
-  add_executable(${ARGV})
-  platformify(${ARGV0})
-  target_link_libraries(${ARGV0} pw_unit_test)
 endfunction()
