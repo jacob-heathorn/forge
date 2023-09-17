b86cbeb1893083f5a66a215232e3b71da2abbb20
Jacob <jacob.heathorn@gmail.com>
2023-09-16 17:34:20 -0700

    Install googletest Install Ninja

--- scripts/python/setup.py
+++ scripts/python/setup.py
@@ -4,12 +4,13 @@
 
 # System pythonmodules
 import argparse
+import subprocess
 import git
 import os
 import shutil
 
 # Custom imports
-from forge.helpers import error
+from forge.helpers import error, pushd
 
 # Pull in environment variables
 FORGE_ROOT = os.environ.get("FORGE_ROOT")
@@ -53,6 +54,7 @@ def clone_and_checkout(repo_url, local_path, tag):
       if tag_ref.name == tag:
         if tag_ref.commit.hexsha == head_commit:
           print(f" - {tag} already checked out")
+          return False
         else:
           repo.git.checkout('tags/' + tag)
           print(f" - Checked out {tag}")
@@ -67,12 +69,57 @@ def clone_and_checkout(repo_url, local_path, tag):
     # Check out the specified tag
     print(f" - Checking out tag {tag}...")
     repo.git.checkout('tags/' + tag)
+  
+  return True
 
   
 def setup_googletest():
   print("Setting up googletest...")
-  clone_and_checkout('https://github.com/google/googletest', f'{FORGE_CACHE}/googletest', 'v1.13.0')
-  print(" - success")
+  clone_dir = f'{FORGE_CACHE}/googletest'
+  build_dir = f'{FORGE_CACHE}/googletest/build'
+  install_dir = f'{FORGE_CACHE}/googletest-install'
+  did_something = clone_and_checkout('https://github.com/google/googletest', clone_dir, 'v1.13.0')
+
+  if did_something:
+    print(f" - Installing to {install_dir}...")
+
+    # Remove build dir if exists
+    if os.path.isdir(build_dir):
+      shutil.rmtree(build_dir)
+
+    # Remove install dir if exists
+    if os.path.isdir(install_dir):
+      shutil.rmtree(install_dir)
+    
+    os.mkdir(install_dir)
+    with pushd(build_dir):
+      args = ['cmake', '..']
+      subprocess.check_call(args)
+      args = ['cmake', '..', f'-DCMAKE_INSTALL_PREFIX={install_dir}']
+      subprocess.check_call(args)
+      subprocess.check_call('make')
+      subprocess.check_call(['make', 'install'])
+      print(" - success")
+
+def is_ninja_installed():
+    try:
+        subprocess.check_call(["ninja", "--version"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        return True
+    except subprocess.CalledProcessError:
+        return False
+    except FileNotFoundError:
+        return False
+
+
+def install_ninja():
+  if not is_ninja_installed():
+    print("Installing ninja...")
+    subprocess.check_call(['sudo', 'apt', 'update'])
+    subprocess.check_call(['sudo', 'apt', 'install', 'ninja-build'])
+    if not is_ninja_installed():
+      error("Failed to install ninja")
+  else:
+    print("Ninja is installed.")
 
 
 
@@ -88,6 +135,10 @@ def main():
   # Repo.clone_from('https://github.com/google/googletest', f'{FORGE_ROOT}/cache/')
   # clone_and_checkout(repo_url, local_path, tag)
 
+  # If no args, assume install
+  if not args.clean:
+    args.install = True
+
   # Do clean
   if args.clean:
     if os.path.exists(FORGE_CACHE):
@@ -100,6 +151,11 @@ def main():
   # Googletest
   if args.install:
     setup_googletest()
+    install_ninja()
+
+
+  # Ninja
+  
      
 
 if __name__ == '__main__':
