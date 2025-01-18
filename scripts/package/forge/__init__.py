aab2762872659853c46d951dfa663eed3cddd809
Jacob <jacob.heathorn@gmail.com>
2025-01-18 01:21:32 -0500

    move vscode generics to forge

--- scripts/package/forge/__init__.py
+++ scripts/package/forge/__init__.py
@@ -3,3 +3,4 @@ from .helpers import error, print_green, remove_file
 from .commands import run
 from .application import Application, NativeApplication
 from .debugger import NativeDebugger
+from .vscode import launch_manager, tasks_manager
---
+++ scripts/package/forge/vscode/__init__.py
@@ -0,0 +1,3 @@
+# flake8: noqa: F401
+from .launch_manager import LaunchManager
+from .tasks_manager import TasksManager
