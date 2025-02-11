d4a8e58f4956a86443f44c12345b4f7f39d853c6
Jacob <jacob.heathorn@gmail.com>
2025-02-11 12:13:28 -0500

    gccArmPath

--- flake.nix
+++ flake.nix
@@ -7,6 +7,7 @@
     system = "x86_64-linux";
     pkgs = import nixpkgs { inherit system; };
     gtestPath = builtins.toString pkgs.gtest.dev.outPath;
+    gccArmPath = builtins.toString pkgs.gcc-arm-embedded-13;
   in {
     devShells.${system}.default = pkgs.mkShell {
       buildInputs = [
@@ -24,6 +25,7 @@
         export CMAKE_PREFIX_PATH=${gtestPath}
         export GTEST_INCLUDE_DIR=${gtestPath}/include
         export PYTHONPYCACHEPREFIX=$PROJECT_ROOT/.pycache
+        export ARM_GCC_TOOLCHAIN_PATH=${gccArmPath}/bin
         echo -e "\033[1;32mWelcome to the forge development shell!\033[0m"
       '';
     };
