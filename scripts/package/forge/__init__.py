64bd1a85b9acc91416deac3fd896009af08d1ad2
Jacob <jacob.heathorn@gmail.com>
2025-02-13 10:26:53 -0500

    first commit

--- scripts/package/forge/__init__.py
+++ scripts/package/forge/__init__.py
@@ -3,3 +3,4 @@ from .helpers import error, print_green, remove_file, pushd
 from .application import Application, NativeApplication
 from .debugger import NativeDebugger
 from .vscode import launch_manager, tasks_manager
+from .cmsis_svd import SVDParserWrapper
---
+++ scripts/package/forge/cmsis_svd/__init__.py
@@ -0,0 +1,2 @@
+# flake8: noqa: F401
+from .svd_parser_wrapper import SVDParserWrapper
