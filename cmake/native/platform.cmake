6f661664b98833e8ec6254e9044714b129cf826d
Jacob <jacob.heathorn@gmail.com>
2025-03-03 06:17:17 -0500

    add_platform_test and add_pw_test

--- cmake/native/platform.cmake
+++ cmake/native/platform.cmake
@@ -4,6 +4,23 @@
 # Include the common platform cmake.
 include(${CMAKE_CURRENT_LIST_DIR}/../common/platform.cmake)
 
+# Adds a gtest unit test executable, which can be executed with ctest.
+function(add_platform_test)
+  find_package(GTest REQUIRED)
+  add_executable(${ARGV})
+  platformify(${ARGV0})
+  target_link_libraries(${ARGV0} GTest::GTest GTest::Main)
+  add_test(NAME ${ARGV0} COMMAND ${ARGV0})
+endfunction()
+
+# Adds a pigweed unit test executable, which can be executed with ctest.
+function(add_pw_test)
+  add_executable(${ARGV})
+  platformify(${ARGV0})
+  target_link_libraries(${ARGV0} PRIVATE pw_unit_test)
+  add_test(NAME ${ARGV0} COMMAND ${ARGV0})
+endfunction()
+
 # Adds platform-specific libraries and options to the target.
 function(platformify target)
   add_common_c_cxx_flags(${target})
