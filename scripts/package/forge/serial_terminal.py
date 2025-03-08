5b11776e8098aa3640299c59aedbbb55072544c8
Jacob <jacob.heathorn@gmail.com>
2025-03-08 09:52:32 -0500

    Cleanup serial terminal

--- scripts/package/forge/serial_terminal.py
+++ scripts/package/forge/serial_terminal.py
@@ -18,6 +18,11 @@ class SerialTerminal:
     self.ser = None
     self.thread = None
 
+    if not SerialTerminal.is_serial_device_available(self.serial_device):
+      forge.error(f"Serial device {self.serial_device} is already in use!")
+
+    self.ser = serial.Serial(self.serial_device, self.baud_rate)
+
   @staticmethod
   def is_serial_device_available(serial_device):
     """Check if a serial port is being used by another process."""
@@ -30,34 +35,35 @@ class SerialTerminal:
       print("lsof command not found. Install it with: sudo apt install lsof")
       return False  # Assume port is free if lsof is unavailable
 
-  def read_serial(self):
+  def readline(self):
     """
-    Continuously read and print serial data.
+    Blocking read and return one line of serial data.
     """
-    if not SerialTerminal.is_serial_device_available(self.serial_device):
-      forge.error(f"Serial device {self.serial_device} is already in use!")
+    return self.ser.readline().decode('utf-8').strip('\r\n')
 
-    else:
-      forge.print_green("Starting serial terminal...")
-      self.ser = serial.Serial(self.serial_device, self.baud_rate)
-      try:
-        while self.ser and self.ser.is_open:
-          line = self.ser.readline().decode('utf-8').strip('\r\n')
-          print(line)
-      except Exception as e:
-        if self.ser and self.ser.is_open:
-          print(f"Serial error: {e}")
-          traceback.print_exc()
-      finally:
-        if self.ser and self.ser.is_open:
-          self.ser.close()
-          print("Serial connection closed.")
+  def read(self):
+    """
+    Blocking continuously read and print serial data.
+    """
+    try:
+      while self.ser and self.ser.is_open:
+        line = self.readline()
+        print(line)
+    except Exception as e:
+      if self.ser and self.ser.is_open:
+        print(f"Serial error: {e}")
+        traceback.print_exc()
+    finally:
+      if self.ser and self.ser.is_open:
+        self.ser.close()
+        print("Serial connection closed.")
 
   def read_background(self):
     """
     Reads the serial terminal in a background thread.
     """
-    self.thread = threading.Thread(target=self.read_serial, daemon=False)
+    forge.print_green("Reading serial terminal in the background...")
+    self.thread = threading.Thread(target=self.read, daemon=False)
     self.thread.start()
     time.sleep(.1)  # For is_serial_device_available() to work immediately.
     signal.signal(signal.SIGINT, self.signal_handler)
