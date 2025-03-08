1e4b4990c3b733639c5c62d46ca8148280f83ff4
Jacob <jacob.heathorn@gmail.com>
2025-03-08 09:41:33 -0500

    Add serial terminal to forge

--- scripts/package/forge/__init__.py
+++ scripts/package/forge/__init__.py
@@ -4,3 +4,4 @@ from .application import Application, NativeApplication
 from .debugger import NativeDebugger
 from .vscode import launch_manager, tasks_manager
 from .cmsis_svd import SVDParserWrapper
+from .serial_terminal import SerialTerminal
