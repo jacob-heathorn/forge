1862ce714fdac71e273ed0477f92da02c6018e90
Jacob Heathorn <jacob.heathorn@gmail.com>
2025-08-08 00:52:18 -0400

    udpate unique allocator

--- firmware/ftl/include/ftl/unique_allocator.hpp
+++ firmware/ftl/include/ftl/unique_allocator.hpp
@@ -13,6 +13,8 @@ namespace ftl {
 template<typename T>
 class UniqueAllocator : public PolymorphicDeleter<T> {
 public:
+    using unique_ptr = std::unique_ptr<T, DelegatingDeleter<T>>;
+
     explicit UniqueAllocator(BumpPoolAllocator2& allocator) noexcept
         : allocator_(allocator) {}
 
@@ -30,12 +32,12 @@ public:
     /// @param args Arguments forwarded to T's constructor
     /// @return unique_ptr that automatically returns memory to pool on destruction
     template<typename... Args>
-    std::unique_ptr<T, DelegatingDeleter<T>> allocate_unique(Args&&... args) {
+    unique_ptr allocate(Args&&... args) {
         // Allocate and construct the object
         T* ptr = allocator_.allocate<T>(std::forward<Args>(args)...);
         
         // Return unique_ptr with delegating deleter pointing to this allocator
-        return std::unique_ptr<T, DelegatingDeleter<T>>(
+        return unique_ptr(
             ptr, 
             DelegatingDeleter<T>(this)
         );
