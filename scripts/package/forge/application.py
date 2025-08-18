ac3fe0db0dc6697f747dc4d587106a6784773004
Jacob Heathorn <jacob.heathorn@gmail.com>
2025-08-18 13:34:39 -0400

    clean keyboard interrupt

--- scripts/package/forge/application.py
+++ scripts/package/forge/application.py
@@ -202,8 +202,12 @@ class NativeApplication(Application):
     print_size(self.application_fullfile)
     print("Running locally...")
     args = [self.application_fullfile]
-    subprocess.check_call(args)
-    forge.print_green("Complete!")
+    try:
+      subprocess.check_call(args)
+      forge.print_green("Complete!")
+    except KeyboardInterrupt:
+      print("\nInterrupted")
+      exit(130)  # Standard exit code for SIGINT
 
   def debug(self):
     debugger = forge.NativeDebugger(name=f"{self.preset_name}:{self.application_name}")
